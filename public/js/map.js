eca7a086b3565d2b07a4017603ed0b4d668732eb
Ayush Negi <a3yushnegi@gmail.com>
2025-02-25 16:41:58 +0530

    map setting

--- public/js/map.js
+++ public/js/map.js
@@ -6,7 +6,7 @@ const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v12",
   center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-  zoom: 9, // starting zoom
+  zoom: 12, // starting zoom
 });
 
 console.log(listing.geometry.coordinates);
